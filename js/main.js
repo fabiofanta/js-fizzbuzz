a74cfe2c68a0a7e077fda49acffbbaae78c7f3fc
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-20 17:52:29 +0100

    1-rel

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/* estrarre un valore da 1 a 100 in ordine crescente
+   controllare se combacia con qualche condizione
+   se si scrivere quella condizione
+   altrimenti scrivere il valore
+*/
+var numbers = [];
+
+for (var i = 1; i <= 100; i++) {
+    // console.log(temp);
+    var resto = i % 3;
+    var restoDue = i % 5;
+    if (resto == 0) {
+        numbers.push("Fizz");
+    } else {
+        numbers.push(i);
+    }
+}
+
+
+// var resto = i % 3;
+// var restoDue = i % 5;
+
+    // if (resto == 0 & restoDue == 5 ) {
+//     console.log("FizzBuzz");
+// } else if (restoDue == 0) {
+//     console.log("Buzz");
+// } else if (resto == 0) {
+//     console.log("Fizz");
+// } else {
+//     numbers.push(i);
+//     console.log(numbers);
+// }
