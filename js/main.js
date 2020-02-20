62559b561023e124676e981858c66456fd68248d
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-20 18:01:49 +0100

    2-rel

--- js/main.js
+++ js/main.js
@@ -9,11 +9,16 @@ for (var i = 1; i <= 100; i++) {
     // console.log(temp);
     var resto = i % 3;
     var restoDue = i % 5;
-    if (resto == 0) {
+    if (resto == 0 && restoDue == 0) {
+        numbers.push("FizzBuzz");
+    } else if (restoDue == 0) {
+        numbers.push("Buzz");
+    } else if (resto == 0) {
         numbers.push("Fizz");
     } else {
         numbers.push(i);
     }
+
 }
 
 
